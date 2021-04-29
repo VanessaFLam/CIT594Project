13a9bec6b5b5b740d1f30d59d6179db6045a76ca
Jackson Golden <jackson.golden@gmail.com>
2021-04-29 15:44:30 -0400

    Javadocs

--- src/Parser.java
+++ src/Parser.java
@@ -17,13 +17,33 @@ import org.json.simple.parser.ParseException;
 public class Parser {
 
     /**
+     * Takes in a csv file with four columns (page_id, item_id, title, views) and copies just
+     * page_id and title to a new csv file.
+     * 
      * @param inFile The path of the csv file to process. The "full" file can be found at
      * "/data/raw/page.csv".
      * @param outFile The path of the csv file to write to. This file should be stored in the
-     * "/data/clean/" directory.
-     * @return
+     * "/data/clean/" directory or a subdirectory thereof.
+     * @return The number of rows successfully parsed (not including header).
      */
-    public static int getPageIds(String inFile, String outFile) {
+    public static int cleanPageCSV(String inFile, String outFile) {
+        return 0;
+    }
+    
+    /**
+     * Parses a JSONL file of links as given by the Wikipedia data and translates to a .mtx file.
+     * This file has a header row of form <# articles> <# links> (i.e., <# vertices> <# edges>) and
+     * data rows of the form <page_id_from> <page_id_to> (i.e., there is a link from the article
+     * given by page_id_from to the article given by page_id_to).
+     * 
+     * @param inFile The path of the JSONL file to process. The "full" file can be found at
+     * "data/raw/link_annotated_text.jsonl".
+     * @param outFile The path of the .mtx file to write to. This file should be stored in the
+     * "/data/clean/" directory or a subdirectory thereof.
+     * @return The number of links successfully parsed. This will be equivalent to the number of
+     * rows in the output file (not including header).
+     */
+    public static int parseLinkData(String inFile, String outFile) {
         return 0;
     }
     
