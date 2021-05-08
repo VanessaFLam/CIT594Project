fd19c40ef48bfe630655adf48a9955cbfac5c03e
davidbinstock <binstock.d@gmail.com>
2021-05-08 10:57:35 -0400

    Added test suite

---
+++ src/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GraphLTest.class, NodeTest.class, WikiGameTest.class })
+public class AllTests {
+
+}
